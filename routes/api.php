8a2922dda1ab7e656095d12fc01ec7605a603c26
Ernesto Canquiz <caribestic123@gmail.com>
2025-02-21 10:28:28 -0400

    feat: Using a shared token

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Middleware\ValidateSharedToken;
 use App\Http\Controllers\GeoLocation\{
     StateController,
     MunicipalityController,
@@ -23,24 +24,21 @@ use App\Http\Controllers\MeasureUnit\{
     return $request->user();
 })->middleware('auth:sanctum');*/
 
-
-
-
-Route::group(['prefix' => 'common'], function() {
+Route::middleware([ValidateSharedToken::class])->group(function () {
     Route::group(['prefix' => 'measure-unit'], function() {
-      Route::get('containers', [MuContainerController::class, 'get']);      
-      Route::get('type', [MuMeasureUnitTypeController::class, 'get']);
-      Route::get('/{muMeasureUnitTypeId}',  [MuMeasureUnitController::class, 'get']);  
-   });
+        Route::get('containers', [MuContainerController::class, 'get']);      
+        Route::get('type', [MuMeasureUnitTypeController::class, 'get']);
+        Route::get('/{muMeasureUnitTypeId}',  [MuMeasureUnitController::class, 'get']);  
+     });
 
-   Route::group(['prefix' => 'geo-location'], function() {
-    Route::get('state', [StateController::class, 'get']);
-    Route::get('municipality/{stateId}', [MunicipalityController::class, 'get']);
-    Route::get('parish/{municipalityId}', [ParishController::class, 'get']);
-    Route::get('city/{municipalityId}', [CityController::class, 'get']);
-    Route::get('zone-type', [ZoneTypeController::class, 'get']);
-    Route::get('route-type', [RouteTypeController::class, 'get']);
-    Route::get('domicile-type', [DomicileTypeController::class, 'get']);
-    Route::get('{ids}', [GeoLocationController::class, 'get']);    
-  });
+    Route::group(['prefix' => 'geo-location'], function() {
+        Route::get('state', [StateController::class, 'get']);
+        Route::get('municipality/{stateId}', [MunicipalityController::class, 'get']);
+        Route::get('parish/{municipalityId}', [ParishController::class, 'get']);
+        Route::get('city/{municipalityId}', [CityController::class, 'get']);
+        Route::get('zone-type', [ZoneTypeController::class, 'get']);
+        Route::get('route-type', [RouteTypeController::class, 'get']);
+        Route::get('domicile-type', [DomicileTypeController::class, 'get']);
+        Route::get('{ids}', [GeoLocationController::class, 'get']);    
+    });
 });
